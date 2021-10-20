a39e1321ef13894e6d5b842244bcb10f75ae2f2f
Justin Elser <elserj@science.oregonstate.edu>
2021-10-20 12:43:44 -0700

    Switch to new inline_diff method to show differences and add pagination

---
+++ genes/templatetags/shared_templatetags.py
@@ -0,0 +1,30 @@
+from django import template
+
+from django.utils.safestring import mark_safe
+
+# difflib import
+import difflib
+
+register = template.Library()
+
+# Note that this is templatetags that are to be shared across apps, but because of
+# Django restrictions, must live in an app.
+
+
+@register.simple_tag
+def inline_diff(a, b):
+    matcher = difflib.SequenceMatcher(None, a, b)
+
+    def process_tag(tag, i1, i2, j1, j2):
+        if tag == 'replace':
+            return '<span style="color:red">' + matcher.a[i1:i2] + '</span><span style="color:green">' + matcher.b[
+                                                                                                         j1:j2] + '</span>'
+        if tag == 'delete':
+            return '<span style="color:red">' + matcher.a[i1:i2] + '</span>'
+        if tag == 'equal':
+            return matcher.a[i1:i2]
+        if tag == 'insert':
+            return '<span style="color:green">' + matcher.b[j1:j2] + '</span>'
+        assert False, "Unknown tag %r" % tag
+
+    return mark_safe(''.join(process_tag(*t) for t in matcher.get_opcodes()))
