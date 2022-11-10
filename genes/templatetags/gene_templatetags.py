ee5fab2f0993902ca04c75649a5b7ac810493937
Justin Elser <elserj@science.oregonstate.edu>
2022-11-10 15:03:35 -0800

    Change aspect and evidence codes to hover text in gene info page. Closes #37

--- genes/templatetags/gene_templatetags.py
+++ genes/templatetags/gene_templatetags.py
@@ -3,9 +3,27 @@ from django import template
 # models import
 from ..models import Gene
 
+# choices import
+import curate.choices as choices
+
 register = template.Library()
 
 
 @register.simple_tag
 def get_gene(key):
     return Gene.objects.get(pk=key)
+
+@register.simple_tag
+def get_aspect(key):
+    aspectChoices=choices.AspectCode
+    for choice in aspectChoices:
+        if key == choice.value:
+            return choice.name
+
+
+@register.simple_tag
+def get_evCode(key):
+    evCodeChoices=choices.EvidenceCode
+    for choice in evCodeChoices:
+        if key == choice.value:
+            return choice.name
