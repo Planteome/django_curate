6e05f94d751bb5c4bb352ae8aba2403ec5bdbf7d
Justin Elser <elserj@science.oregonstate.edu>
2023-12-06 16:08:45 -0800

    Update Django to 4.2.8 and python to 3.11. Required small change to "ALLOWED_HOSTS" in settings.py and change versions in requirements.txt. Closes #69.

--- curate/settings.py
+++ curate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = list(os.environ.get('ALLOWED_HOSTS'))
 
 
 # Application definition
